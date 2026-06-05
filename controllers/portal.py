7272e12ed95d9b899a38cc24b5f4c15b5784ad0a
AnshMNSoni <anshsoni702@gmail.com>
2026-06-05 12:40:15 +0530

    [FIX] Fix the issue of cyclomatic complexity

--- controllers/portal.py
+++ controllers/portal.py
@@ -38,11 +38,44 @@ class Website(Website):
             'standards': standards,
         })
 
+    def _validate_student_registration(self, post):
+        name = post.get('name')
+        roll_number = post.get('roll_number')
+        email = post.get('email')
+        standard_id = post.get('standard_id')
+        password = post.get('password')
+        confirm_password = post.get('confirm_password')
+
+        if not name or not roll_number or not email or not standard_id or not password or not confirm_password:
+            return 'Please fill all required fields.'
+
+        if password != confirm_password:
+            return 'Passwords do not match.'
+
+        existing_user = request.env['res.users'].sudo().search([('login', '=', email)], limit=1)
+        if existing_user:
+            return 'A user with this email is already registered.'
+
+        existing_student = request.env['student.management'].sudo().search([('roll_number', '=', roll_number)], limit=1)
+        if existing_student:
+            return 'Roll Number must be unique. A student with this roll number is already registered.'
+
+        return None
+
     @http.route('/student/register/submit', type='http', auth='public', methods=['POST'], website=True, csrf=True)
     def student_register_submit(self, **post):
         user = request.env.user
         if not user._is_public() and user.has_group('student_management.group_teacher') and not user.has_group('student_management.group_admin'):
             return request.render('website.403')
+
+        error = self._validate_student_registration(post)
+        if error:
+            standards = request.env['student.standard'].sudo().search([])
+            return request.render('student_management.student_registration_form_temp', {
+                'standards': standards,
+                'error': error,
+                'post': post
+            })
 
         name = post.get('name')
         roll_number = post.get('roll_number')
@@ -51,42 +84,7 @@ class Website(Website):
         standard_id = post.get('standard_id')
         admission_date = post.get('admission_date')
         password = post.get('password')
-        confirm_password = post.get('confirm_password')
         subject_ids = request.httprequest.form.getlist('subject_ids')
-
-        if not name or not roll_number or not email or not standard_id or not password or not confirm_password:
-            standards = request.env['student.standard'].sudo().search([])
-            return request.render('student_management.student_registration_form_temp', {
-                'standards': standards,
-                'error': 'Please fill all required fields.',
-                'post': post
-            })
-
-        if password != confirm_password:
-            standards = request.env['student.standard'].sudo().search([])
-            return request.render('student_management.student_registration_form_temp', {
-                'standards': standards,
-                'error': 'Passwords do not match.',
-                'post': post
-            })
-
-        existing_user = request.env['res.users'].sudo().search([('login', '=', email)], limit=1)
-        if existing_user:
-            standards = request.env['student.standard'].sudo().search([])
-            return request.render('student_management.student_registration_form_temp', {
-                'standards': standards,
-                'error': 'A user with this email is already registered.',
-                'post': post
-            })
-
-        existing_student = request.env['student.management'].sudo().search([('roll_number', '=', roll_number)], limit=1)
-        if existing_student:
-            standards = request.env['student.standard'].sudo().search([])
-            return request.render('student_management.student_registration_form_temp', {
-                'standards': standards,
-                'error': 'Roll Number must be unique. A student with this roll number is already registered.',
-                'post': post
-            })
 
         try:
             student_vals = {
