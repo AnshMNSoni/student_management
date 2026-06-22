3046b656418d68c02f556cb023551962a53cf72a
AnshMNSoni <anshsoni702@gmail.com>
2026-06-22 13:14:30 +0530

    fix(sales.order) remove the unwanted content from pop up page

--- static/src/product_catalog/duplicate_product_dialog.js
+++ static/src/product_catalog/duplicate_product_dialog.js
@@ -13,8 +13,9 @@ export class DuplicateProductDialog extends Component {
         productName: String,
         qtyAdded: Number,
         lines: Array,
+        isDecrease: { type: Boolean, optional: true },
         onUpdateExisting: Function,
-        onAddAsNewLine: Function,
+        onAddAsNewLine: { type: Function, optional: true },
         onCancel: Function,
         title: { type: String, optional: true },
     };
@@ -27,7 +28,7 @@ export class DuplicateProductDialog extends Component {
         this.env.dialogData.dismiss = () => this._cancel();
         this.state = useState({
             selectedLineId: this.props.lines && this.props.lines.length > 0 ? this.props.lines[0].id : null,
-            showLinePicker: false,
+            showLinePicker: this.props.isDecrease || false,
         });
     }
 
@@ -57,6 +58,10 @@ export class DuplicateProductDialog extends Component {
     }
 
     _goBack() {
-        this.state.showLinePicker = false;
+        if (this.props.isDecrease) {
+            this._cancel();
+        } else {
+            this.state.showLinePicker = false;
+        }
     }
 }
